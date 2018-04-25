259760c3a95f5ab7985fd891db575789d195a83a
devendra-ghate <devendra.ghate@gmail.com>
2018-04-25 14:37:26 +0530

    added polynomial regression model.

--- SurrogateModels.jl
+++ SurrogateModels.jl
@@ -1,8 +1,23 @@
 module SurrogateModels
 
+"""
+    regression(X,y,m)
+
+Output a regression matrix given inputs
+
+ - `X`: ``n × k`` matrix with `n` training points in `k` dimensional feature space
+
+ - `y`: ``n × 1`` vector of responses (######)
+
+ - `m`: Order of polynomial (Presently only `1` and `2` is supported)
+
+"""
 function regression(x,y,m)
 
+    return(x)
+end
 
+end
 # vim:filetype=julia:foldmethod=syntax
 
 
